e164dfdec90efc5df612092b1e9698164266d3ba
changu02 <changu02@naver.com>
2018-01-04 18:10:54 +0900

    hello world

---
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by lse0101 on 2015. 1. 21..
+ */
+@RestController
+@EnableAutoConfiguration
+public class Demo1Application {
+
+    @RequestMapping("/")
+    public String home(){
+        return "Hello World";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo1Application.class, args);
+    }
+}
